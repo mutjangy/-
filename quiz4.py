d7aa989f36b2044a33cd04eca467a2a285ef5b71
mutjangy <97266816+mutjangy@users.noreply.github.com>
2022-09-12 21:24:14 +0900

    Add files via upload

---
+++ quiz4.py
@@ -0,0 +1,17 @@
+from random import *
+
+users = range(1,21) # 1 부터 20까지 숫자를 생성
+#print(type(users))
+users = list(users)
+#print(type(users))
+
+print(users)
+shuffle(users)
+print(users)
+
+winners = sample(users, 4) # 4명 중에서 1명은 치킨, 3명은 커피 
+
+print("--당첨자 발표--")
+print("치킨 당첨자 : {0}".format(winners[0]))
+print("커피 당첨자 : {0}".format(winners[1:]))
+print("--축하합니다--")
