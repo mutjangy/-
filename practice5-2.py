e7e6dbb0c9c085d298299aaa21051f0e4e91c6bd
mutjangy <97266816+mutjangy@users.noreply.github.com>
2022-09-13 23:41:20 +0900

    Add files via upload

---
+++ practice5-2.py
@@ -0,0 +1,5 @@
+#print("대기번호 : 1")
+#print("대기번호 : 2")
+
+for waiting_no in range(1,6): # 1,2,3,4,5
+    print("대기번호 : {0}".format(waiting_no))
---
+++ practice5-5.py
@@ -0,0 +1,8 @@
+#무한루프
+#종료하려면 control c 누르면 됨
+
+customer = "아이언맨"
+index = 1
+while True:
+    print("{0}, 커피가 준비 되었습니다. 호출 {1} 회".format(customer, index))
+    index += 1
